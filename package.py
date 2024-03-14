5c2923419090596eff94fb7238ff0b5d82348060
Marcelo Sercheli <msercheli@gmail.com>
2024-03-14 16:45:15 -0400

    Adjusted for Rocky

--- package.py
+++ package.py
@@ -12,7 +12,6 @@ description = """Text shaping engine"""
 with scope("config") as c:
     import os
     c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
-    #c.build_thread_count = "physical_cores"
 
 requires = [
     "freetype",
@@ -23,7 +22,6 @@ private_build_requires = [
 ]
 
 variants = [
-    ["platform-linux", "arch-x86_64", "os-centos-7"],
 ]
 
 # If want to use Ninja, run:
@@ -37,7 +35,23 @@ variants = [
 uuid = "repository.harfbuzz"
 
 def pre_build_commands():
-    command("source /opt/rh/devtoolset-6/enable")
+
+    info = {}
+    with open("/etc/os-release", 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            line_info = line.replace('\n', '').split('=')
+            if len(line_info) != 2:
+                continue
+            info[line_info[0]] = line_info[1].replace('"', '')
+    linux_distro = info.get("NAME", "centos")
+    print("Using Linux distro: " + linux_distro)
+
+    if linux_distro.lower().startswith("centos"):
+        command("source /opt/rh/devtoolset-6/enable")
+    elif linux_distro.lower().startswith("rocky"):
+        pass
 
 def commands():
     # NOTE: REZ package versions can have ".sse." to separate the external
